f49dd51d3e420d067a02e5a43bd59099d3165ea9
bhratsharma01 <bhratsharma619@gmail.com>
2024-11-04 21:36:53 +0530

    Delete frontend directory

--- frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-/* src/index.js */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './styles.css';  // Import your custom CSS file
-
-ReactDOM.render(<App />, document.getElementById('root'));
